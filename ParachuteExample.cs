b38b074c2c4eb6f05270d11e31411594ffc4ff64
Giovanny Espinal Ramírez <giovanny.espinal@gmail.com>
2021-07-22 15:13:47 -0500

    Add files via upload

---
+++ ParachuteExample.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParachuteExample : MonoBehaviour {
+
+    Rigidbody rigidbody;
+    Renderer renderer;
+    ParticleSystem ps;
+
+    void Start() {
+        rigidbody = GetComponent<Rigidbody>();
+        renderer = GetComponent<Renderer>();
+        ps = GetComponentInChildren<ParticleSystem>();
+    }
+
+    void Update() {
+
+        if (Input.GetButtonDown("Jump")) {
+            rigidbody.drag = 10;
+            renderer.material.color = Color.red;
+            ps.Play();
+        }
+
+        if (Input.GetButtonUp("Jump")) {
+            rigidbody.drag = 0;
+            renderer.material.color = Color.white;
+            ps.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        }
+        
+    }
+}
